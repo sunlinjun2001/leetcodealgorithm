da2b27584605e41e9aceb33b78414153df9ee6d2
sunlinjun20010110 <sunlinjun20010110@163.com>
2023-09-03 18:03:21 +0800

    88.合并两个有序数组

--- array/mergesortedarray.cpp
+++ array/mergesortedarray.cpp
@@ -3,9 +3,9 @@
 #include <algorithm>
 //88.合并两个有序数组
 /* 给你两个按非递减顺序排列的整数数组nums1和nums2，另有两个整数m和n ，分别表示nums1和nums2中的元素数目。
-请你 合并 nums2 到 nums1 中，使合并后的数组同样按 非递减顺序 排列。
+请你合并nums2到nums1中，使合并后的数组同样按非递减顺序排列。
 注意：最终，合并后数组不应由函数返回，而是存储在数组nums1中。
-为了应对这种情况，nums1 的初始长度为m+n，其中前m个元素表示应合并的元素，后n个元素为0 ，应忽略。nums2的长度为n  */
+为了应对这种情况，nums1 的初始长度为m+n，其中前m个元素表示应合并的元素，后n个元素为0 ，应忽略。nums2的长度为n */
 class Solution {
 public:
     void merge(std::vector<int>& nums1, int m, std::vector<int>& nums2, int n) {
