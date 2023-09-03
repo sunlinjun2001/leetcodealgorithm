e990bb0839d9611330ecde10ca36fafef4ee478c
sunlinjun20010110 <sunlinjun20010110@163.com>
2023-09-03 17:59:10 +0800

    35. 搜索插入位置

--- array/searchInsertIndex.cpp
+++ array/searchInsertIndex.cpp
@@ -1,7 +1,7 @@
 #include<iostream>
 using namespace std;
 #include <vector>
-//2.搜索插入位置
+//35. 搜索插入位置
 //给定一个排序数组和一个目标值，在数组中找到目标值，并返回其索引。如果目标值不存在于数组中，返回它将会被按顺序插入的位置
 //请必须使用时间复杂度为 O(log n) 的算法。
 class Solution {
